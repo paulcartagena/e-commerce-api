1f3da1deb37809ef8caba3a6e5b776c8e0cd0f46
paulcartagena <paulcartagena04@gmail.com>
2026-07-29 23:39:04 -0600

    initial commit

---
+++ src/main/java/com/paulcartagena/watchstore/WatchStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.paulcartagena.watchstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchStoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatchStoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paulcartagena/watchstore/WatchStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulcartagena.watchstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
