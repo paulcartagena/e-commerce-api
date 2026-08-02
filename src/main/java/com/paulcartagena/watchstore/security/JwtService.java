86a0a61c157104cc9cd350859f7029a142c72736
paulcartagena <paulcartagena04@gmail.com>
2026-08-02 00:24:32 -0600

    feat: implement jwt generation

--- src/main/java/com/paulcartagena/watchstore/security/JwtService.java
+++ src/main/java/com/paulcartagena/watchstore/security/JwtService.java
@@ -1,10 +1,12 @@
 package com.paulcartagena.watchstore.security;
 
 import com.paulcartagena.watchstore.auth.entity.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -34,8 +36,32 @@ public class JwtService {
                 .compact();
     }
 
+    public String getUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        return getUsername(token).equals(userDetails.getUsername())
+                && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractAllClaims(token)
+                .getExpiration()
+                .before(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
     private SecretKey getSigningKey() {
-        byte [] keyBytes = Decoders.BASE64.decode(jwtSecret);
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
