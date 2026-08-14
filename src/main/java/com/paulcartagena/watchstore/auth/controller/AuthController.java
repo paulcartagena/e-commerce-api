29f67a48747aeeedc09d2815a5596cd0acdd4398
paulcartagena <paulcartagena04@gmail.com>
2026-08-13 23:59:43 -0600

    add initial AuthController

--- src/main/java/com/paulcartagena/watchstore/auth/controller/AuthController.java
+++ src/main/java/com/paulcartagena/watchstore/auth/controller/AuthController.java
@@ -1,4 +1,15 @@
 package com.paulcartagena.watchstore.auth.controller;
 
+import com.paulcartagena.watchstore.auth.service.AuthService;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
 }
